ca0ba780a272868a8469068eb0fe6707d6723490
itsgabeover <gabe.enrique.miranda@gmail.com>
2024-04-23 00:15:28 -0400

    initial commit

---
+++ backend/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('notes/', views.NoteListCreate.as_view(), name='note-list'),
+    path('notes/<int:pk>/', views.NoteDelete.as_view(), name='note-delete')
+]
